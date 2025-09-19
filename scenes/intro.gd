e41a49bbfa674eefc94e3a60b2890c08a436da17
Dario De Vita <devitadario@gmail.com>
2025-09-19 23:17:51 +0200

    feat: main menu skip animation

--- scenes/intro.gd
+++ scenes/intro.gd
@@ -2,10 +2,27 @@
 extends Control
 
 
+var skip_delta: float = 0.0
+const SKIP_TIME: float = 0.5
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed(&"ui_cancel"):
+		skip_delta += delta
+	elif skip_delta > 0.0:
+		skip_delta -= delta
+		skip_delta = min(0.0, skip_delta)
+	
+	%lb_skip_highlight.size.x = %lb_skip.size.x * (skip_delta/SKIP_TIME)
+	
+	if skip_delta > SKIP_TIME:
+		skip()
+
+
 func skip() -> void:
 	Mng.go_to_main_menu()
 
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed(&"ui_cancel"):
-		skip()
+#callback from the animation
+func _on_intro_anim_finished() -> void:
+	Mng.go_to_main_menu()
