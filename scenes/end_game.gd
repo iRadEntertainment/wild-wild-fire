0e8818f99638b711c1b9b9cb7cb5eea118d26189
Dario De Vita <devitadario@gmail.com>
2025-09-30 18:21:35 +0200

    feat: added end scene

--- scenes/end_game.gd
+++ scenes/end_game.gd
@@ -1,1 +1,8 @@
 extends Control
+
+
+
+
+func _on_btn_return_pressed() -> void: Mng.go_to_main_menu()
+func _on_btn_credits_pressed() -> void: %pnl_credits.show()
+func _on_btn_quit_pressed() -> void: Mng.quit()
